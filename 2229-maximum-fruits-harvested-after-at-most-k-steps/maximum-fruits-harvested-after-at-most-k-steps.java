2c0f7aa5a8787cdaee89f1c9a67eeabdae086de4
shivaamaroju <naninaidu.0369@gmail.com>
2025-08-06 10:48:02 +0530

    Time: 16 ms (30.51%) | Memory: 107.7 MB (6.47%) - LeetSync

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.java
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.java
@@ -1,43 +1,35 @@
 class Solution {
     public int maxTotalFruits(int[][] fruits, int startPos, int k) {
-        int maxReach = Math.max(startPos + k, fruits[fruits.length - 1][0]);
-        int[] Actual = new int[maxReach + 1];
-        Map<Integer, Integer> mp = new HashMap<>();
+        // Find last fruit position to size the array
+        int last = fruits[fruits.length - 1][0];
+        int[] actual = new int[Math.max(last, startPos) + k + 2];
 
+        // Fill in the actual fruit count at each position
         for (int[] fruit : fruits) {
-            mp.put(fruit[0], fruit[1]);
+            actual[fruit[0]] = fruit[1];
         }
 
-        for (int i = 0; i <= maxReach; i++) {
-            if (mp.containsKey(i)) {
-                Actual[i] = mp.get(i);
-            }
-        }
-
-        int[] prefix = new int[Actual.length];
-        prefix[0] = Actual[0];
-        for (int i = 1; i < Actual.length; i++) {
-            prefix[i] = prefix[i - 1] + Actual[i];
+        // Build prefix sum for fast range queries
+        for (int i = 1; i < actual.length; i++) {
+            actual[i] += actual[i - 1];
         }
 
         int max = 0;
 
-        for (int i = 0; i <= k; i++) {
-            // Left i, right k - 2i
-            int left = Math.max(0, startPos - i);
-            int right = Math.min(prefix.length - 1, startPos + (k - 2 * i));
-            if (left <= right) {
-                int sum1 = prefix[right] - (left > 0 ? prefix[left - 1] : 0);
-                max = Math.max(max, sum1);
-            }
+        // Move left first (i steps left, then j steps right)
+        for (int x = 0; x <= k / 2; x++) {
+            int left = startPos - x;
+            int right = Math.min(startPos + (k - 2 * x), actual.length - 1);
+            int total = actual[right] - (left > 0 ? actual[left - 1] : 0);
+            max = Math.max(max, total);
+        }
 
-            // Right i, left k - 2i
-            right = Math.min(prefix.length - 1, startPos + i);
-            left = Math.max(0, startPos - (k - 2 * i));
-            if (left <= right) {
-                int sum2 = prefix[right] - (left > 0 ? prefix[left - 1] : 0);
-                max = Math.max(max, sum2);
-            }
+        // Move right first (i steps right, then j steps left)
+        for (int x = 0; x <= k / 2; x++) {
+            int right = startPos + x;
+            int left = Math.max(0, startPos - (k - 2 * x));
+            int total = actual[right] - (left > 0 ? actual[left - 1] : 0);
+            max = Math.max(max, total);
         }
 
         return max;
