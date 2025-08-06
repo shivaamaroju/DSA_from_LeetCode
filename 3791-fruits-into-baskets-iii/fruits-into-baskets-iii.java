f0cafd9516beaf7331409f300cafcd3a6cb49d45
shivaamaroju <naninaidu.0369@gmail.com>
2025-08-06 14:23:37 +0530

    Time: 97 ms (41.44%) | Memory: 65.2 MB (45.05%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.java
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.java
@@ -1,66 +1,62 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        NumsTree tree = new NumsTree(baskets);
-        int unplaced = 0;
-
-        for (int fruit : fruits) {
-            int pos = tree.place(fruit);
-            if (pos == -1) unplaced++;
+        Tree obj=new Tree(baskets.length,baskets);
+        int c=0;
+        for(var s: fruits){
+int idx=obj.place(s);
+if(idx==-1)c++;
         }
-        return unplaced;
+        return c;
     }
 }
-
-class NumsTree {
-    int[] seg;
+class Tree{
     int n;
-
-    public NumsTree(int[] baskets) {
-        n = baskets.length;
-        seg = new int[4 * n];
-        build(0, 0, n - 1, baskets);
+    int seg[];
+    public Tree(int n,int a[]){
+        this.n=n;
+    seg=new int[4*n];
+build(0,0,n-1,a);
+// for(var s:seg){
+//     System.out.print(s+" ");
+// }
     }
-
-    private void build(int idx, int l, int r, int[] baskets) {
-        if (l == r) {
-            seg[idx] = baskets[l];
+    public void build(int idx,int l,int h,int a[]){
+        if(l==h){
+            seg[idx]=a[l];
             return;
         }
-        int mid = (l + r) / 2;
-        build(2 * idx + 1, l, mid, baskets);
-        build(2 * idx + 2, mid + 1, r, baskets);
-        seg[idx] = Math.max(seg[2 * idx + 1], seg[2 * idx + 2]); 
+        int mid=(l+h)/2;
+        build(2*idx+1,l,mid,a);
+        build(2*idx+2,mid+1,h,a);
+        seg[idx]=Math.max(seg[2*idx+1],seg[2*idx+2]);
     }
-
-    public int place(int val) {
-        int pos = query(0, 0, n - 1, val);
-        if (pos != -1) {
-            update(0, 0, n - 1, pos, 0); 
+    public int place(int val){
+        int x=helper(0,0,n-1,val);
+        if(x!=-1){
+           update(0,0,n-1,x);
+           return x;
         }
-        return pos;
+        return x;
     }
-
-    private int query(int idx, int l, int r, int val) {
-        if (seg[idx] < val) return -1; 
-
-        if (l == r) return l;
-
-        int mid = (l + r) / 2;
-        int left = query(2 * idx + 1, l, mid, val);
-        if (left != -1) return left;
-        return query(2 * idx + 2, mid + 1, r, val);
+    public int helper(int idx,int l,int h,int val){
+        if(seg[idx]<val)return -1;
+        if(l==h){
+            return l;
+        }
+        int mid=(l+h)/2;
+        int left=helper(2*idx+1,l,mid,val);
+        if(left!=-1)return left;
+        int right=helper(2*idx+2,mid+1,h,val);
+        return right;
     }
-
-    private void update(int idx, int l, int r, int pos, int val) {
-        if (l == r) {
-            seg[idx] = val;
+    public void update(int idx,int l,int h,int pos){
+        if(l==h){
+            seg[idx]=0;
             return;
         }
-
-        int mid = (l + r) / 2;
-        if (pos <= mid) update(2 * idx + 1, l, mid, pos, val);
-        else update(2 * idx + 2, mid + 1, r, pos, val);
-
-        seg[idx] = Math.max(seg[2 * idx + 1], seg[2 * idx + 2]);
+        int mid=(l+h)/2;
+        if(pos<=mid)update(2*idx+1,l,mid,pos);
+        else update(2*idx+2,mid+1,h,pos);
+        seg[idx]=Math.max(seg[2*idx+1],seg[2*idx+2]);
     }
 }
