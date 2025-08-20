f2ca67e36ff18b710c0271e2c73eec79f0dc4e2f
shivaamaroju <naninaidu.0369@gmail.com>
2025-08-20 11:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (60.86%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,23 +1,17 @@
 class Solution {
-    int c=0;
     public int uniquePaths(int m, int n) {
-        int a[][]=new int[m][n];
-        boolean vis[][]=new boolean[m][n];
-        int dp[][]=new int [m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+        int[] aboveRow = new int[n];
+        Arrays.fill(aboveRow, 1);
+
+        for (int row = 1; row < m; row++) {
+            int[] currentRow = new int[n];
+            Arrays.fill(currentRow, 1);
+            for (int col = 1; col < n; col++) {
+                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+            }
+            aboveRow = currentRow;
         }
-        return 
-        helper(a,m,n,0,0,dp);
-    }
-    public int helper(int a[][],int m, int n,int i,int j,int dp[][]){
-        if(i>=m||j>=n)return  0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(i==m-1&&j==n-1){
-            return 1;
-        }
-      int x=  helper(a,m,n,i+1,j,dp);//down
-       int y= helper(a,m,n,i,j+1,dp);//up
-       return dp[i][j]=x+y;
+
+        return aboveRow[n - 1];        
     }
 }
