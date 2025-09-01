f9c2a6bbd248616fd3660aa4f2f5438e9d2387f1
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-01 20:21:19 +0530

    Time: 572 ms (7.26%) | Memory: 75.1 MB (99.48%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.java
@@ -1,31 +1,26 @@
 class Solution {
-    public double maxAverageRatio(int[][] classes, int extraStudents) {
-        // PriorityQueue based on the gain of adding one passing student
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> {
-            double gain1 = ((double)(x[0] + 1) / (x[1] + 1)) - ((double)x[0] / x[1]);
+    public double maxAverageRatio(int[][] c, int e) {
+        
+        PriorityQueue<int []> pq=new PriorityQueue<>((x,y)->{
+ double gain1 = ((double)(x[0] + 1) / (x[1] + 1)) - ((double)x[0] / x[1]);
             double gain2 = ((double)(y[0] + 1) / (y[1] + 1)) - ((double)y[0] / y[1]);
-            return Double.compare(gain2, gain1); // max-heap behavior
+            return Double.compare(gain2, gain1);
         });
-
-        for (int[] c : classes) {
-            pq.add(c);
+        for(var s:c)pq.add(s);
+        while(e-->0){
+        int a[]=pq.remove();
+        a[0]=a[0]+1;
+        a[1]=a[1]+1;
+        pq.add(a);
         }
-
-        // Assign extra students greedily
-        while (extraStudents-- > 0) {
-            int[] top = pq.poll();
-            top[0]++; // add one pass
-            top[1]++; // add one student
-            pq.add(top);
+        double sum=0.0;
+        while(pq.size()>0){
+            int a[]=pq.remove();
+            sum=sum+(double)a[0]/a[1];
         }
+        return sum/c.length;
 
-        // Calculate average ratio
-        double sum = 0.0;
-        while (!pq.isEmpty()) {
-            int[] a = pq.poll();
-            sum += (double)a[0] / a[1];
-        }
 
-        return sum / classes.length;
+
     }
 }
