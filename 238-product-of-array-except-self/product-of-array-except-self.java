61860359ab79f29c209d41c5dbb1d1b073e4b479
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-16 06:58:52 +0530

    Time: 2 ms (87.19%) | Memory: 56.3 MB (19.62%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
-        int[] res = new int[n];
-
-        prefix[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] * nums[i];
+        int n=nums.length;
+        int p[]=new int[n];
+        int s[]=new int[n];
+        int r[]=new int[n];
+        p[0]=nums[0];
+        for(int i=1;i<n;i++){
+            p[i]=p[i-1]*nums[i];
         }
-        suffix[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] * nums[i];
+        s[n-1]=nums[n-1];
+        for(int j=n-2;j>=0;j--){
+            s[j]=s[j+1]*nums[j];
         }
-
-        for (int i = 0; i < n; i++) {
-            int left = (i > 0) ? prefix[i - 1] : 1;
-            int right = (i < n - 1) ? suffix[i + 1] : 1;
-            res[i] = left * right; // ✅ multiplication, not division
+        for(int i=0;i<n;i++){
+            int l=(i-1)>=0 ? p[i-1] : 1;
+            int re=(i+1<n) ? s[i+1] : 1;
+            r[i]=l*re;
         }
-
-        return res;
+        return r;
     }
 }
