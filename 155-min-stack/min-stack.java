3dc9ea2cc815c63552126a9d8986469257814945
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-18 05:26:57 +0530

    Time: 5 ms (39.08%) | Memory: 44.9 MB (60.21%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,22 +1,20 @@
 class MinStack {
-Stack<Integer>st;
+Stack<Integer> st;
 Stack<Integer> min;
     public MinStack() {
         st=new Stack<>();
         min=new Stack<>();
-
     }
     
     public void push(int val) {
-        st.push(val);
-        if(min.isEmpty())min.push(val);
-        else min.push(Math.min(val,min.peek()));
+    st.push(val);
+    if(min.isEmpty())min.push(val);
+    else min.push(Math.min(val,min.peek()));
     }
     
     public void pop() {
         st.pop();
         min.pop();
-        
     }
     
     public int top() {
@@ -24,7 +22,7 @@ Stack<Integer> min;
     }
     
     public int getMin() {
-        return min.peek();
+    return min.peek();  
     }
 }
 
