a4899e649f736b78a0ec5c081eb2729031d947fd
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-18 08:00:01 +0530

    Time: 93 ms (38.80%) | Memory: 47.8 MB (25.86%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,38 +1,34 @@
 class Solution {
     public int ladderLength(String b, String e, List<String> wordList) {
-        HashSet<String> hs = new HashSet<>(wordList);
-        if (!hs.contains(e)) return 0; // end word must be in wordList
-
-        HashSet<String> vis = new HashSet<>();
-        Queue<String> q = new LinkedList<>();
-        int c = 1; // start from 1 because beginWord is counted
-        q.add(b);
+        HashSet<String> hs=new HashSet<>(wordList);
+        if(!hs.contains(e))return 0;
+        Queue<String> q=new LinkedList<>();
+        HashSet<String> vis=new HashSet<>();
         vis.add(b);
+        q.add(b);
+        int c=1;
+        while(q.size()>0){
+int n=q.size();
+while(n-->0){
+    String s=q.remove();
+    char ch[]=s.toCharArray();
+    for(int i=0;i<ch.length;i++){
+        char or=ch[i];
+        for(char j='a';j<='z';j++){
+if(j==or)continue;
+ch[i]=j;
+String ss=new String(ch);
+if(ss.equals(e))return c+1;
+if(hs.contains(ss)&&!vis.contains(ss)){
+    q.add(ss);
+    vis.add(ss);
+}
+ch[i]=or;
+        }
+    }
 
-        while (!q.isEmpty()) {
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                String x = q.remove();
-                char[] ch = x.toCharArray();
-
-                for (int j = 0; j < ch.length; j++) {
-                    char or = ch[j];
-                    for (char k = 'a'; k <= 'z'; k++) {
-                        if (k == or) continue; // skip same character
-                        ch[j] = k;
-                        String kotha = new String(ch);
-
-                        if (kotha.equals(e)) return c + 1;
-
-                        if (!vis.contains(kotha) && hs.contains(kotha)) {
-                            q.add(kotha);
-                            vis.add(kotha);
-                        }
-                    }
-                    ch[j] = or;
-                }
-            }
-            c++;
+}
+c++;
         }
         return 0;
     }
