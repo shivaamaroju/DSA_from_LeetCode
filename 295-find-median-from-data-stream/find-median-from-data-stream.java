2808948363f6ffefefd40c2f3e9b0e05fb37ddf4
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-19 22:31:24 +0530

    Time: 101 ms (79.72%) | Memory: 63.5 MB (82.04%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,36 +1,22 @@
 class MedianFinder {
 PriorityQueue<Integer> max;
 PriorityQueue<Integer> min;
-
     public MedianFinder() {
-        max=new PriorityQueue<>((x,y)->{
-            return y-x;
-        });
-       min =new PriorityQueue<>((x,y)->{
-            return x-y;
-        });
+        max=new PriorityQueue<>((x,y)-> x-y);
+        min=new PriorityQueue<>((x,y)->  y-x);
     }
     
     public void addNum(int num) {
-        if(max.isEmpty()||(num<=max.peek()))max.add(num);
+        if(max.isEmpty()||max.peek()<num)max.add(num);
         else min.add(num);
-        if(max.size()>min.size()+1)min.add(max.remove());
-        else if(min.size()>max.size()) {
-            max.add(min.remove());
-        }
-
-        
+        if(max.size()-1>min.size())min.add(max.remove());
+        if(min.size()>max.size())max.add(min.remove());
     }
     
     public double findMedian() {
-      if(max.size()==min.size())return (max.peek()+min.peek())/2.0;
-      else return max.peek();
+        if(max.size()==min.size())return (max.peek()+min.peek())/2.0;
+        return max.peek();
+        
     }
 }
 
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
- */
