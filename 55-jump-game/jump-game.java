65f03a90bc3a357766511037c5b81156b334ca58
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-21 00:33:03 +0530

    Time: 351 ms (6.93%) | Memory: 46 MB (21.74%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        if(nums.length==1)return true;
         Boolean dp[]=new Boolean[nums.length];
         return helper(nums,0,dp);
     }
     public boolean helper(int nums[],int idx,Boolean dp[]){
-        if(idx>=nums.length-1) return true;
+        if(idx>=nums.length-1)return true;
         if(dp[idx]!=null)return dp[idx];
         for(int i=1;i<=nums[idx];i++){
-          dp[idx] =  helper(nums,idx+i,dp);
-          if(dp[idx])return true;
-         
+            if(helper(nums,idx+i,dp))return dp[idx]=true;
         }
-        return false;
+        return dp[idx]=false;
     }
 }
