ebff54e7b25695a62d530b16fea2f4dd5c74bf40
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-20 16:05:21 +0530

    Time: 37 ms (47.61%) | Memory: 55.2 MB (86.09%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,54 +1,56 @@
 class Trie {
- Node root;
+Node root;
     public Trie() {
-        root=new Node();
+     root=new Node();   
     }
     
-    public void insert(String s) {
+    public void insert(String word) {
         Node temp=root;
-for(int i=0;i<s.length();i++){
-    int x=(int)s.charAt(i)-'a';
-    if(temp.a[x]==null){
-        temp.a[x]=new Node();
-    }
-    temp=temp.a[x];
+        for(int i=0;i<word.length();i++){
+            int ch=(int)word.charAt(i)-'a';
+            if(temp.a[ch]==null){
+                temp.a[ch]=new Node();
+            }
+            temp=temp.a[ch];
 
-}
-temp.ew=true;
-        
-    }
-    
-    public boolean search(String s) {
-           Node temp=root;
-for(int i=0;i<s.length();i++){
-    int x=(int)s.charAt(i)-'a';
-    if(temp.a[x]==null){
-       return false;
-    }
-    temp=temp.a[x];
 
-}
-return temp.ew;
+
+
+
+
+            }
+            temp.ew=true;
     }
     
-    public boolean startsWith(String s) {
-      Node temp=root;
-for(int i=0;i<s.length();i++){
-    int x=(int)s.charAt(i)-'a';
-    if(temp.a[x]==null){
-       return false;
+    public boolean search(String word) {
+       Node temp=root;
+        for(int i=0;i<word.length();i++){
+            int ch=(int)word.charAt(i)-'a';
+            if(temp.a[ch]==null)return false;
+            temp=temp.a[ch];
+        }
+        return temp.ew;
     }
-    temp=temp.a[x];
-
-}
-return true;
+    
+    public boolean startsWith(String prefix) {
+       Node temp=root;
+       for(int i=0;i<prefix.length();i++){
+        int ch=(int)prefix.charAt(i)-'a';
+        if(temp.a[ch]==null)return false;
+        temp=temp.a[ch];
+       } 
+       return true;
     }
 }
-
-
-
 class Node{
     Node a[]=new Node[26];
-    boolean ew =false;
-
+    boolean ew=false;
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
