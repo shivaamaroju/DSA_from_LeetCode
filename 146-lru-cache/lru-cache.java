b0aa157e1a6b59623376504b476f35554615d319
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-21 00:48:59 +0530

    Time: 47 ms (50.14%) | Memory: 113.9 MB (56.54%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,86 +1,28 @@
 class LRUCache {
-HashMap<Integer,Node>mp;
-link obj;
-int size;
-int ca;
+LinkedHashMap<Integer,Integer>mp;
+int c;
     public LRUCache(int capacity) {
-       mp=new HashMap<>();
-       obj=new link();
-       size=0;
-       ca=capacity;
-
+      c=capacity;  
+      mp=new LinkedHashMap<>(c,0.75f,true);
     }
     
     public int get(int key) {
-        if(!mp.containsKey(key)){
-return -1;
-        }
-        Node x=mp.get(key);
-        obj.movefront(x);
-        return x.val;
-
+        return mp.getOrDefault(key,-1);
     }
     
-    public void put(int key, int val) {
-        if(mp.containsKey(key)){
-            Node x = mp.get(key); // get existing node
-    x.val = val;          
-            mp.put(key,x);
-            obj.movefront(x);
-            return;
+    public void put(int key, int value) {
+        if(mp.containsKey(key))
+        mp.put(key,value);
+        else{
+            if(mp.size()==c){
+int l=mp.keySet().iterator().next();
+mp.remove(l);
+            }
+            mp.put(key,value);
         }
-        if(size==ca){
-            Node x=obj.delete();
-            mp.remove(x.key);
-            size--;
-        }
-        Node x=new Node(key,val);
-        obj.insert(x);
-        mp.put(key,x);
-        size++;
-
     }
 }
-class Node{
-    Node next;
-    Node prev;
-    int key;
-    int val;
-    Node(){
 
-    }
-    Node(int key,int val){
-        this.key=key;
-        this.val=val;
-    }
-}
-class link{
-    Node head,tail;
-    link(){
-        head=new Node();
-        tail=new Node();
-        head.next=tail;
-        tail.prev=head;
-    }
-    public void insert(Node x){
-        x.next=head.next;
-        head.next.prev=x;
-    head.next=x;
-    x.prev=head;
-    }
-    public void movefront(Node x){
-        x.prev.next=x.next;
-        x.next.prev=x.prev;
-        insert(x);
-    }
-    public Node delete(){
-        Node x=tail.prev;
-        x.prev.next=tail;
-        tail.prev=x.prev;
-        return x;
-
-    }
-}
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
