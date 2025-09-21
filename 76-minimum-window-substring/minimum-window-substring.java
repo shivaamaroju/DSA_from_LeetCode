ecc963ebe8a86c64594c90a77308d9e969ae3725
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-21 20:34:35 +0530

    Time: 17 ms (61.17%) | Memory: 44.8 MB (80.89%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,31 +1,33 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character,Integer>mp=new HashMap<>();
-        int min=Integer.MAX_VALUE,ws=-1,st=0;
-        for(var ss : t.toCharArray()){
+        HashMap<Character,Integer> mp=new HashMap<>();
+        for(var ss:t.toCharArray()){
             mp.put(ss,mp.getOrDefault(ss,0)+1);
         }
-        int stillneed=t.length();
+        int still=t.length();
+        int min=Integer.MAX_VALUE;
+        int ws=-1;
+        int st=0;
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(mp.containsKey(ch)){
-             if(mp.get(ch)>0)stillneed--;
-             mp.put(ch,mp.get(ch)-1); 
+                if(mp.get(ch)>0)still--;
+                mp.put(ch,mp.get(ch)-1);
             }
-            while(stillneed==0&&st<=i){
+            while(still==0){
                 if(i-st+1<min){
                     min=i-st+1;
                     ws=st;
                 }
-                char ch2=s.charAt(st);
+                char ch3=s.charAt(st);
                 st++;
-if(mp.containsKey(ch2)){
-    mp.put(ch2,mp.get(ch2)+1);
-    if(mp.get(ch2)>0)stillneed++;
-}
+                if(mp.containsKey(ch3)){
+                    mp.put(ch3,mp.get(ch3)+1);
+                    if(mp.get(ch3)>0)still++;
+                }
             }
-        }
 
+        }
         if(ws==-1)return "";
         return s.substring(ws,ws+min);
     }
