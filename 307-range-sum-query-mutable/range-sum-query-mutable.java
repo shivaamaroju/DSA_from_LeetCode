46f7c9766d1bf5ba317ccfc7e1113ecc0aca258c
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-22 06:54:54 +0530

    Time: 93 ms (52.77%) | Memory: 78.4 MB (12.82%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/range-sum-query-mutable.java
@@ -1,10 +1,10 @@
 class NumArray {
-sum obj;
 int n;
+sum obj;
     public NumArray(int[] nums) {
-      n=nums.length;
-        obj=new sum(nums.length);
-        obj.build(nums,0,nums.length-1,0);
+        n=nums.length;
+        obj=new sum(n);
+       obj.build(nums,0,n-1,0);
     }
     
     public void update(int index, int val) {
@@ -20,34 +20,33 @@ class sum{
     sum(int n){
         seg=new int[4*n];
     }
-public void build(int nums[],int l,int h,int idx){
-    if(l==h){
-        seg[idx]=nums[l];
-        return;
+    public void build(int nums[],int l, int h,int idx){
+        if(l==h){
+            seg[idx]=nums[l];
+        return;}
+        int mid=(l+h)/2;
+        build(nums,l,mid,2*idx+1);
+        build(nums,mid+1,h,2*idx+2);
+        seg[idx]=seg[2*idx+1]+seg[2*idx+2];
     }
-    int mid=(l+h)/2;
-    build(nums,l,mid,2*idx+1);
-    build(nums,mid+1,h,2*idx+2);
-    seg[idx]=seg[2*idx+1]+seg[2*idx+2];
-}
-public void update(int nums[],int l,int h,int pos,int val,int idx){
-    if(l==h){
-        seg[idx]=val;
-        return;
+    public void update(int nums[],int l, int h,int pos,int val,int idx){
+        if(l==h){
+            seg[idx]=val;
+            return;
+        }
+        int mid=(l+h)/2;
+      if(pos<=mid)  update(nums,l,mid,pos,val,2*idx+1);
+      else update(nums,mid+1,h,pos,val,2*idx+2);
+      seg[idx]=seg[2*idx+1]+seg[2*idx+2];
     }
-    int mid=(l+h)/2;
-    if(pos<=mid)update(nums,l,mid,pos,val,2*idx+1);
-    else update(nums,mid+1,h,pos,val,2*idx+2);
-    seg[idx]=seg[2*idx+1]+seg[2*idx+2];
-}
-public int get(int nums[],int l,int r,int left,int right,int idx){
-    if(left>r||right<l)return 0;
-    if(l>=left&&r<=right)return seg[idx];
-    int mid=(l+r)/2;
-    int x=get(nums,l,mid,left,right,2*idx+1);
-    int y=get(nums,mid+1,r,left,right,2*idx+2);
-    return x+y;
-}
+    public int get(int nums[],int l,int r,int left,int right,int idx){
+        if(left>r||right<l)return 0;
+        if(l>=left&&r<=right)return seg[idx];
+        int mid=(l+r)/2;
+        int x=get(nums,l,mid,left,right,2*idx+1);
+        int y=get(nums,mid+1,r,left,right,2*idx+2);
+        return x+y;
+    }
 }
 /**
  * Your NumArray object will be instantiated and called as such:
