793c7a5a56f0e11ec4728a2299a5d73e2852e82f
shivaamaroju <naninaidu.0369@gmail.com>
2025-09-24 06:39:44 +0530

    Time: 461 ms (33.95%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -2,7 +2,8 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
 set N=N-1;
   RETURN (
-      select distinct e.salary from Employee  as e order by e.salary  desc limit N, 1
+    
+select distinct e.salary from Employee as e order by  e.salary desc limit N, 1
 
   );
 END
