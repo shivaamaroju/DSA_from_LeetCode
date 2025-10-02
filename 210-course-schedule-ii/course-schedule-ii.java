0e6258cf6fe9031ceb6d34926e5ede4e8eeae142
shivaamaroju <naninaidu.0369@gmail.com>
2025-10-02 10:29:39 +0530

    Time: 7 ms (26.14%) | Memory: 45.7 MB (81.91%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,40 +1,31 @@
 class Solution {
     public int[] findOrder(int n, int[][] p) {
-    int a[]=new int[n];
-    HashMap<Integer,ArrayList<Integer>> mp=new HashMap<>();
-    for(int i=0;i<n;i++){
-        mp.put(i,new ArrayList<>());
-    }
-    for(var s: p){
-    mp.get(s[1]).add(s[0]);
-    a[s[0]]++;
-    }
-    Queue<Integer> q=new LinkedList<>();
-    HashSet<Integer> st=new HashSet<>();
-    for(int i=0;i<n;i++){
-        if(a[i]==0){
-            q.add(i);
-            st.add(i);
+        HashMap<Integer,List<Integer>> mp=new HashMap<>();
+        int in[]=new int[n];
+        for(int i=0;i<p.length;i++){
+            mp.computeIfAbsent(p[i][1],k->new ArrayList<>()).add(p[i][0]);
+            in[p[i][0]]++;
         }
-    }
-int b[]=new int [n];
-int i=0;
+        Queue<Integer> q=new LinkedList<>();
+List<Integer> l=new ArrayList<>();
+for(int i=0;i<n;i++){
+if(in[i]==0)q.add(i);
+}
 while(q.size()>0){
     int x=q.remove();
-    b[i++]=x;
-    for(var s:mp.get(x)){
-        a[s]--;
-        if(a[s]==0){
-            q.add(s);
-            st.add(s);
-        }
+    l.add(x);
+    if(mp.containsKey(x)){
+for(int s:mp.get(x)){
+    in[s]--;
+    if(in[s]==0)q.add(s);
+}
     }
 }
-return i==n ?b :new int[0];
-
-
-
-
-
+int a[]=new int[n];
+if(l.size()!=n)return new int[]{};
+for(int i=0;i<n;i++){
+    a[i]=l.get(i);
+}
+return a;
     }
 }
