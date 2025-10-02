d99919ffcbd43fd41c4697b1c0d1cc3d9b1f2f46
shivaamaroju <naninaidu.0369@gmail.com>
2025-10-02 11:12:26 +0530

    Time: 97 ms (32.18%) | Memory: 47.8 MB (27.14%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,34 +1,34 @@
 class Solution {
-    public int ladderLength(String b, String e, List<String> wordList) {
-        HashSet<String> hs=new HashSet<>(wordList);
+    public int ladderLength(String b, String e, List<String> l) {
+        HashSet<String> hs=new HashSet<>(l);
+        HashSet<String> vis=new HashSet<>();
         if(!hs.contains(e))return 0;
         Queue<String> q=new LinkedList<>();
-        HashSet<String> vis=new HashSet<>();
-        vis.add(b);
         q.add(b);
+        vis.add(b);
         int c=1;
         while(q.size()>0){
-int n=q.size();
-while(n-->0){
-    String s=q.remove();
-    char ch[]=s.toCharArray();
-    for(int i=0;i<ch.length;i++){
-        char or=ch[i];
-        for(char j='a';j<='z';j++){
-if(j==or)continue;
-ch[i]=j;
-String ss=new String(ch);
-if(ss.equals(e))return c+1;
-if(hs.contains(ss)&&!vis.contains(ss)){
-    q.add(ss);
-    vis.add(ss);
-}
-ch[i]=or;
-        }
-    }
+            int n=q.size();
+            while(n-->0){
+            String s=q.remove();
+            char a[]=s.toCharArray();
 
-}
-c++;
+            for(int i=0;i<a.length;i++){
+                char or=a[i];
+                for(char ch='a';ch<='z';ch++){
+                    if(ch==or)continue;
+                    a[i]=ch;
+                    String t=new String(a);
+                    if(t.equals(e))return c+1;
+                    if(!vis.contains(t)&&hs.contains(t)){
+                        vis.add(t);
+                        q.add(t);
+
+                    }
+                    a[i]=or;
+                }
+            }}
+            c++;
         }
         return 0;
     }
