5e2e5738b0bb8fb2e8cf4f9082a03ebe6db3470c
shivaamaroju <naninaidu.0369@gmail.com>
2025-10-09 08:35:10 +0530

    Time: 75 ms (6.32%) | Memory: 70.9 MB (17.30%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,28 +1,21 @@
-import java.util.*;
-
 class Solution {
-    public int findMinArrowShots(int[][] points) {
-        if(points.length == 0) return 0;
+    public int findMinArrowShots(int[][] a) {
+        if(a.length==0)return 0;
+        Arrays.sort(a,(x,y)-> Integer.compare(x[1],y[1]));
+        List<List<Integer>> l=new ArrayList<>();
+        for(var s:a){
+            if(l.size()>0&&l.get(l.size()-1).get(1)>=s[0]){
+               l.get(l.size()-1).set(1,Math.min(s[1],l.get(l.size()-1).get(1)));
+               }
+                else{
+ List<Integer> ll=new ArrayList<>();
+                ll.add(s[0]);
+                ll.add(s[1]);
+                l.add(ll);
+                }
 
-        Arrays.sort(points, (x, y) -> Integer.compare(x[1], y[1]));
-
-        List<List<Integer>> l = new ArrayList<>();
-        List<Integer> ll = new ArrayList<>();
-        ll.add(points[0][0]);
-        ll.add(points[0][1]);
-        l.add(ll);
-
-        for(int i = 1; i < points.length; i++){
-            if(l.get(l.size()-1).get(1) >= points[i][0]){
-                l.get(l.size()-1).set(1, Math.min(l.get(l.size()-1).get(1), points[i][1]));
-            } else {
-                List<Integer> lll = new ArrayList<>();
-                lll.add(points[i][0]);
-                lll.add(points[i][1]);
-                l.add(lll);
-            }
         }
-
         return l.size();
+
     }
 }
