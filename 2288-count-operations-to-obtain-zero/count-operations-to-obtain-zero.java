78da7548bba5b0efb7fddf8586ce1cfcd836e80f
shivaamaroju <naninaidu.0369@gmail.com>
2025-11-09 12:53:37 +0530

    Time: 1 ms (90.35%) | Memory: 42.1 MB (18.76%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -1,26 +1,16 @@
-import java.util.*;
-
 class Solution {
-    HashMap<String, Integer> mp = new HashMap<>();
+    public int countOperations(int num1, int num2) {
+        int c = 0;
+        while (num1 != 0 && num2 != 0) {
+            if (num1 >= num2) {
+                num1 -= num2;
+                c++;
+            } else {
+                num2 -= num1;
+                c++;
+            }
 
-    public int countOperations(int n, int m) {
-        return helper(n, m);
-    }
-
-    private int helper(int n, int m) {
-        // Base cases
-        if (n == 0 || m == 0) return 0;
-
-        String key = n + "," + m;
-        if (mp.containsKey(key)) return mp.get(key);
-
-        int ans;
-        if (n >= m)
-            ans = 1 + helper(n - m, m);
-        else
-            ans = 1 + helper(n, m - n);
-
-        mp.put(key, ans);
-        return ans;
+        }
+        return c;
     }
 }
