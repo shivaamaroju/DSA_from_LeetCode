2e1d2cda0e88ce53858d2e5e5c620069fd7fb7be
shivaamaroju <naninaidu.0369@gmail.com>
2025-11-11 19:27:52 +0530

    Time: 10 ms (14.72%) | Memory: 47 MB (13.04%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean canFinish(int n, int[][] p) {
+        List<Integer> l=new ArrayList<>();
+        int f[]=new int[n];
         HashMap<Integer,List<Integer>> mp=new HashMap<>();
-        int in[]=new int[n];
-        for(var s:p){
-            mp.computeIfAbsent(s[1],k->new ArrayList<>()).add(s[0]);
-        in[s[0]]++;
+        for(int i=0;i<p.length;i++){
+            int x=p[i][0];
+            int y=p[i][1];
+f[x]++;
+mp.computeIfAbsent(y,k->new ArrayList()).add(x);
+    }
+    Queue<Integer>q=new LinkedList<>();
+    for(int i=0;i<n;i++)if(f[i]==0)q.add(i);
+    while(q.size()>0){
+        int x=q.remove();
+        l.add(x);
+        if(!mp.containsKey(x))continue;
+        for(int s:mp.get(x)){
+            f[s]--;
+            if(f[s]==0)q.add(s);
         }
-        Queue<Integer> q=new LinkedList<>();
-        int c=0;
-        for(int i=0;i<n;i++)if(in[i]==0)q.add(i);
-        while(q.size()>0){
-            int x=q.remove();
-            c++;
-        if(mp.containsKey(x)){
-            for(int s:mp.get(x)){
-                in[s]--;
-                if(in[s]==0)q.add(s);
-            }
+    }
+    return l.size()==n;
         }
-        }
-
-    return c==n;}
 }
