f7e3cb98e0dac58133d7a663b4b89c58a19999a5
shivaamaroju <naninaidu.0369@gmail.com>
2025-11-16 11:07:21 +0530

    Time: 6 ms (23.30%) | Memory: 46.3 MB (9.16%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -1,20 +1,28 @@
 class Solution {
     public int numSub(String s) {
-        long c=0;
-        long oc=0;
-        for(int i=0;i<s.length();i++){
-            int  x=s.charAt(i)-'0';
-            if(x==0){
-                c=0;
+        int oc = 0;
+        long c = 0;
+        long MOD = 1000000007;
+
+        for(int i = 0; i < s.length(); i++){
+            int x = s.charAt(i) - '0';
+
+            if(x == 0){
+                // FIXED: compute c*(c+1)/2 BEFORE modulo
+                long val = (c * (c + 1)) / 2;
+                oc = (int)((oc + (val % MOD)) % MOD);
+
+                c = 0;   // reset counter of consecutive 1s
             }
             else{
                 c++;
-                oc+=c;
             }
-
         }
-              
-                return (int)(oc%((int)1e9+7));
 
+        // Final block outside loop
+        long val = (c * (c + 1)) / 2;
+        oc = (int)((oc + (val % MOD)) % MOD);
+
+        return oc;
     }
 }
