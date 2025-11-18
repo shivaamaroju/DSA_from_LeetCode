a1da00965e0696755defa028f6cd39419811083c
shivaamaroju <naninaidu.0369@gmail.com>
2025-11-18 22:27:55 +0530

    Time: 9 ms (22.02%) | Memory: 47.3 MB (6.49%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,25 +1,28 @@
 class Solution {
     public boolean canFinish(int n, int[][] p) {
-        List<Integer> l=new ArrayList<>();
-        int f[]=new int[n];
         HashMap<Integer,List<Integer>> mp=new HashMap<>();
-        for(int i=0;i<p.length;i++){
-            int x=p[i][0];
-            int y=p[i][1];
-f[x]++;
-mp.computeIfAbsent(y,k->new ArrayList()).add(x);
+        List<Integer> ans=new ArrayList<>();
+        int in[]=new int[n];
+for(var s:p){
+            mp.computeIfAbsent(s[1],k->new ArrayList<>()).add(s[0]);
+            in[s[0]]++;
+}
+Queue<Integer> q=new LinkedList<>();
+for(int i=0;i<n;i++)if(in[i]==0)q.add(i);
+while(q.size()>0){
+    int x=q.remove();
+ans.add(x);
+if(!mp.containsKey(x))continue;
+for(var s:mp.get(x)){
+    in[s]--;
+    if(in[s]==0)q.add(s);
+}
+}
+return ans.size()==n;
+
+
+
+
+
     }
-    Queue<Integer>q=new LinkedList<>();
-    for(int i=0;i<n;i++)if(f[i]==0)q.add(i);
-    while(q.size()>0){
-        int x=q.remove();
-        l.add(x);
-        if(!mp.containsKey(x))continue;
-        for(int s:mp.get(x)){
-            f[s]--;
-            if(f[s]==0)q.add(s);
-        }
-    }
-    return l.size()==n;
-        }
 }
