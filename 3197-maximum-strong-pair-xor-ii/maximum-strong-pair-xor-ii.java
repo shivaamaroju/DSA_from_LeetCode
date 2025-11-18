c5c9dfe1b989739bb8d5b89cc6fd56b5dc4173fd
shivaamaroju <naninaidu.0369@gmail.com>
2025-11-18 21:18:55 +0530

    Time: 162 ms (25.49%) | Memory: 144.9 MB (9.80%) - LeetSync

--- 3197-maximum-strong-pair-xor-ii/maximum-strong-pair-xor-ii.java
+++ 3197-maximum-strong-pair-xor-ii/maximum-strong-pair-xor-ii.java
@@ -1,57 +1,65 @@
 class Solution {
     public int maximumStrongPairXor(int[] nums) {
-        Tree obj=new Tree();
         Arrays.sort(nums);
-        int st=0,max=0;
+        int st=0;
+        Trie obj=new Trie();
+        int max=0;
         for(int i=0;i<nums.length;i++){
-while(st<i&&Math.abs(nums[st]-nums[i])>Math.min(nums[st],nums[i])){
-    obj.remove(nums[st++]);
-}
-obj.insert(nums[i]);
-max=Math.max(max,obj.max(nums[i]));
+            while(st<i&&Math.abs(nums[st]-nums[i])>Math.min(nums[st],nums[i])){
+obj.remove(nums[st++]);
+            }
+            obj.build(nums[i]);
+            max=Math.max(max,obj.find(nums[i]));
+
         }
         return max;
     }
 }
 class Node{
     Node a[]=new Node[2];
+    boolean ew=false;
     int c=0;
 }
-class Tree{
-Node root=new Node();
-public void insert(int n){
-    Node temp=root;
-    for(int i=31;i>=0;i--){
-        int x=(n>>i)&1;
-        if(temp.a[x]==null)temp.a[x]=new Node();
-        temp=temp.a[x];
-        temp.c++;
-    }
-}
-public void remove(int n){
-    Node temp=root;
-    for(int i=31;i>=0;i--){
-        int x=(n>>i)&1;
-        temp=temp.a[x];
-        temp.c--;
-    }
-}
-public int max(int n){
-     Node temp=root;
-     int res=0;
-    for(int i=31;i>=0;i--){
-        int x=(n>>i)&1;
-        int y=1-x;
-        if(temp.a[y]!=null&&temp.a[y].c>0){
-            res|=(1<<i);
-            temp=temp.a[y];
-        }
-       else if(temp.a[x]!=null&&temp.a[x].c>0){
+class Trie{
+    Node root=new Node();
+    public void build(int n){
+        Node temp=root;
+        for(int i=31;i>=0;i--){
+            int x=(n>>i)&1;
+            if(temp.a[x]==null){
+                temp.a[x]=new Node();
+            }
             temp=temp.a[x];
+            temp.c++;
         }
-       else return res;
-    }
-    return res;
-}
+        temp.ew=true;
 
+    }
+    public void remove(int n){
+         Node temp=root;
+        for(int i=31;i>=0;i--){
+            int x=(n>>i)&1;
+            temp=temp.a[x];
+            temp.c--;
+        }
+       
+    }
+    public int find(int n){
+         Node temp=root;
+         int xor=0;
+        for(int i=31;i>=0;i--){
+            int x=(n>>i)&1;
+            int op=1-x;
+         if(temp.a[op]!=null&&temp.a[op].c>0){
+            xor|=(1<<i);
+            temp=temp.a[op];}
+          else if(temp.a[x]!=null&&temp.a[x].c>0){
+          
+            temp=temp.a[x];}
+            else return xor;
+           
+        }
+        return xor;
+       
+    }
 }
