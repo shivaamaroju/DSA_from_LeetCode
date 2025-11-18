c88ec243b6a5d8d41926c660e7c8b946f7d8cc9e
shivaamaroju <naninaidu.0369@gmail.com>
2025-11-18 13:17:37 +0530

    Time: 132 ms (34.22%) | Memory: 111.8 MB (10.96%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.java
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.java
@@ -1,62 +1,52 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        Tree obj=new Tree(baskets.length,baskets);
+        SEG obj=new SEG(baskets);
         int c=0;
-        for(var s: fruits){
-int idx=obj.place(s);
-if(idx==-1)c++;
+        for(int s:fruits){
+            int pos=obj.query(0,0,baskets.length-1,s);
+            if(pos!=-1){
+                obj.update(0,0,baskets.length-1,pos,0);
+
+            }
+            else c++;
+
         }
         return c;
     }
 }
-class Tree{
-    int n;
-    int seg[];
-    public Tree(int n,int a[]){
-        this.n=n;
-    seg=new int[4*n];
-build(0,0,n-1,a);
-// for(var s:seg){
-//     System.out.print(s+" ");
-// }
+class SEG{
+    int n,seg[];
+    SEG(int nums[]){
+        n=nums.length;
+        seg=new int[4*n];
+        build(0,0,n-1,nums);
     }
-    public void build(int idx,int l,int h,int a[]){
+    public void build(int idx,int l,int h,int nums[]){
         if(l==h){
-            seg[idx]=a[l];
+            seg[idx]=nums[l];
             return;
         }
         int mid=(l+h)/2;
-        build(2*idx+1,l,mid,a);
-        build(2*idx+2,mid+1,h,a);
+        build(2*idx+1,l,mid,nums);
+        build(2*idx+2,mid+1,h,nums);
         seg[idx]=Math.max(seg[2*idx+1],seg[2*idx+2]);
     }
-    public int place(int val){
-        int x=helper(0,0,n-1,val);
-        if(x!=-1){
-           update(0,0,n-1,x);
-           return x;
-        }
-        return x;
+    public int query(int idx,int l,int h,int pos){
+        if(seg[idx]<pos)return -1;
+        if(l==h)return l;
+        int mid =(l+h)/2;
+       int left= query(2*idx+1,l,mid,pos);
+       if(left!=-1)return left;
+    return query(2*idx+2,mid+1,h,pos);
     }
-    public int helper(int idx,int l,int h,int val){
-        if(seg[idx]<val)return -1;
-        if(l==h){
-            return l;
-        }
-        int mid=(l+h)/2;
-        int left=helper(2*idx+1,l,mid,val);
-        if(left!=-1)return left;
-        int right=helper(2*idx+2,mid+1,h,val);
-        return right;
-    }
-    public void update(int idx,int l,int h,int pos){
+    public void update(int idx,int l,int h,int pos,int val){
         if(l==h){
             seg[idx]=0;
             return;
         }
         int mid=(l+h)/2;
-        if(pos<=mid)update(2*idx+1,l,mid,pos);
-        else update(2*idx+2,mid+1,h,pos);
+        if(pos<=mid)update(2*idx+1,l,mid,pos,val);
+        else update(2*idx+2,mid+1,h,pos,val);
         seg[idx]=Math.max(seg[2*idx+1],seg[2*idx+2]);
     }
 }
