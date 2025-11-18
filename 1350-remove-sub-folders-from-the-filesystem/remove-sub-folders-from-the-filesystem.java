57497883f82382b77ca43f61c5db201409493c3c
shivaamaroju <naninaidu.0369@gmail.com>
2025-11-18 22:18:55 +0530

    Time: 69 ms (23.34%) | Memory: 61.8 MB (20.82%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.java
@@ -1,33 +1,32 @@
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        Trie obj=new Trie();
-        Arrays.sort(folder);
-        List<String> l=new ArrayList<>();
-        for(var s: folder){
-            if(obj.insert(s))l.add(s);
-        }
-        return l;
-
+       Arrays.sort(folder); 
+       Trie obj=new Trie();
+       List<String> ans=new ArrayList<>();
+      for(var s:folder){
+        if(obj.build(s))ans.add(s);
+      } 
+      return ans;
     }
 }
 class Node{
-    HashMap<String,Node> mp=new HashMap<>();
-    boolean ew=false;
+HashMap<String,Node> mp=new HashMap<>();
+boolean ew=false;
 }
 class Trie{
-    Node  root=new Node();
-    public boolean insert(String s){
-Node temp=root;
-String a[]=s.split("/");
-for(int i=0;i<a.length;i++){
-    if(temp.ew)return false;
-   if(!temp.mp.containsKey(a[i])){
-    temp.mp.put(a[i],new Node());
-   } 
-   temp=temp.mp.get(a[i]);
+    Node root=new Node();
+public boolean build(String s){
+        Node temp=root;
+        String ss[]=s.split("/");
+for(int i=0;i<ss.length;i++){
+if(temp.ew)return false;
+String ch=ss[i];
+if(!temp.mp.containsKey(ch)){
+temp.mp.put(ch,new Node());
 }
-temp.ew=true;
-return true;
+temp=temp.mp.get(ch);
+        }
+        temp.ew=true;
+        return true;
     }
-
 }
