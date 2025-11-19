5f4a85295306fa834bc388f416c6d3cd43e1d8c8
shivaamaroju <naninaidu.0369@gmail.com>
2025-11-19 06:04:27 +0530

    Time: 124 ms (8.10%) | Memory: 156.1 MB (5.63%) - LeetSync

--- 3738-make-array-non-decreasing/make-array-non-decreasing.java
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.java
@@ -1,14 +1,13 @@
 class Solution {
     public int maximumPossibleSize(int[] nums) {
-        int c=0;
-        Stack<Integer> st=new Stack<>();
-        for(var s:nums){
-            int cur=s;
-            while(!st.isEmpty()&&st.peek()>cur){
-                cur=Math.max(cur,st.pop());
-            }
-            st.push(cur);
+     Stack<Integer> st=new Stack<>();
+     for(var s:nums){
+        int cur =s;
+        while(!st.isEmpty()&&st.peek()>cur){
+            cur=Math.max(st.pop(),cur);
         }
-        return st.size();
+        st.push(cur);
+     }
+     return st.size();
     }
 }
