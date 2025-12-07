55f50b0ee9de33fa35a73e149303a01f3dc4ec44
shivaamaroju <naninaidu.0369@gmail.com>
2025-12-07 15:23:01 +0530

    Time: 13 ms (0.00%) | Memory: 43.7 MB (0.00%) - LeetSync

---
+++ 4100-complete-prime-number/complete-prime-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean completePrime(int num) {
+ 
+    
+    StringBuffer sb=new StringBuffer(""+num);
+    for(int i=0;i<sb.length();i++){
+        int x=Integer.parseInt(sb.substring(0,i+1));
+        int y=Integer.parseInt(sb.substring(sb.length()-i-1));
+        System.out.println(x+" "+y);
+        if(!prime(x)||!prime(y))return false;
+
+    }
+    return true;
+
+    }
+    public boolean prime(int x){
+        if(x<2)return false;
+        for(int i=2;i<=Math.sqrt(x);i++){
+            if(x%i==0)return false;
+        }
+        return true;
+    }
+}
