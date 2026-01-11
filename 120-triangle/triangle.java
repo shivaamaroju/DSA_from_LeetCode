f365cfef664f866165bb0020faa647d97ba8503c
shivaamaroju <naninaidu.0369@gmail.com>
2026-01-11 07:37:17 +0530

    Time: 9 ms (16.38%) | Memory: 45.8 MB (39.54%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,14 +1,26 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> l) {
-        for (int i = l.size() - 2; i >= 0; i--) {
-            for (int j = 0; j < l.get(i).size(); j++) {
-                int min = Math.min(
-                    l.get(i + 1).get(j),
-                    l.get(i + 1).get(j + 1)
-                );
-                l.get(i).set(j, l.get(i).get(j) + min);
+    public int minimumTotal(List<List<Integer>> t) {
+        int dp[][]=new int[t.size()][t.size()];
+        dp[0][0]=t.get(0).get(0);
+        for(int i=1;i<t.size();i++){
+            for(int j=0;j<t.get(i).size();j++){
+int d=0,dg=0;
+if(j<t.get(i-1).size())d=t.get(i).get(j)+dp[i-1][j];
+else d+=(int)1e9;
+if(j-1>=0)dg+=t.get(i).get(j)+dp[i-1][j-1];
+else dg+=(int)1e9;
+dp[i][j]=Math.min(d,dg);
             }
         }
-        return l.get(0).get(0);
+        int ans=(int)(1e9);
+for(int i=0;i<t.get(t.size()-1).size();i++){
+    ans=Math.min(ans,dp[t.size()-1][i]);
+}
+return ans;
+
     }
+    
+    
+
+    
 }
