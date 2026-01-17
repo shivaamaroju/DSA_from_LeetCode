9fcab41511c26e8e2abb0433a50abbc96f93ef99
shivaamaroju <naninaidu.0369@gmail.com>
2026-01-17 14:23:05 +0530

    Time: 6 ms (10.88%) | Memory: 50 MB (84.25%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,19 +1,26 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int dp[][]=new int[grid.length][grid[0].length];
-        for(int i=0;i<grid.length;i++){
-            Arrays.fill(dp[i],-1);
+    public int minPathSum(int[][] a) {
+        int m=a.length;
+        int n=a[0].length;
+        int dp[][]=new int[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int up=(int)1e9,down=(int)1e9;
+                if(i-1>=0)up=dp[i-1][j];
+                if(j-1>=0)down=dp[i][j-1];
+                int min=Math.min(up,down);
+               if(min!=1e9)dp[i][j]=min;
+               dp[i][j]+=a[i][j];
+            }
         }
-        return helper(grid,0,0,grid.length,grid[0].length,dp);
+        return dp[m-1][n-1];
     }
-    public int helper(int a[][],int i,int j,int m,int n, int dp[][]){
-        if(i>=m||j>=n)return Integer.MAX_VALUE;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(i==m-1&&j==n-1){
-           return a[i][j];
-        }
-       int x= helper(a,i+1,j,m,n,dp);
-       int y= helper(a,i,j+1,m,n,dp);
-     return  dp[i][j]=a[i][j]+Math.min(x,y);
-    }
+    public int fun(int a[][],int i,int j){
+
+if(i==0&&j==0)return a[i][j];
+if(i<0||j<0)return (int)1e9;
+int up=a[i][j]+fun(a,i-1,j);
+int down=a[i][j]+fun(a,i,j-1);
+return Math.min(up,down);}
 }
