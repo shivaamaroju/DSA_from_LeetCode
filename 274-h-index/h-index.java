de03c65f608a3548c0d5477512595acb43ae8e50
shivaamaroju <naninaidu.0369@gmail.com>
2026-02-18 07:02:47 +0530

    Time: 5 ms (69.00%) | Memory: 43 MB (98.11%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int hIndex(int[] a) {
+        int n=a.length;
+        Arrays.sort(a);
+        int l=a[0],h=a[n-1];
+        int ans=n;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(fun(mid,a)){
+ans=mid;
+l=mid+1;
+            }
+            else h=mid-1;
+        }
+        return ans;
+    }
+    public boolean fun(int mid,int a[]){
+        int c=0;
+        for(int s:a){
+            if(s>=mid)c++;
+            if(c>=mid)return true;
+        }
+        return c>=mid;
+    }
+}
